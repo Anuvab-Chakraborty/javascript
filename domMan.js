b7911e99cdfe8454056bc1a97e55d06c6f9ef969
Anuvab-Chakraborty <anuvab.xd@gmail.com>
2023-07-30 19:28:47 +0530

    domManipulation Updated

--- domMan.js
+++ domMan.js
@@ -1,3 +1,4 @@
-let card_T=document.getElementsByClassName("card-title")[0]
+// let card_T=document.getElementsByClassName("card-title")[0]
+let card_T=document.getElementById("firstCardTitle")
 console.log(card_T);
 card_T.style.color="red"
