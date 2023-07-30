ed73f701a350ea78dbc1a25753c8622c4e056c38
Anuvab-Chakraborty <anuvab.xd@gmail.com>
2023-07-30 20:09:41 +0530

    dom manipulation methods added

--- domMan.js
+++ domMan.js
@@ -1,4 +1,14 @@
 // let card_T=document.getElementsByClassName("card-title")[0]
-let card_T=document.getElementById("firstCardTitle")
-console.log(card_T);
-card_T.style.color="red"
+// let card_T=document.getElementById("firstCardTitle")
+// let card_T=document.querySelectorAll(".card-title")
+// console.log(card_T);
+// card_T[0].style.color="red";
+// card_T[1].style.color="blue"
+// card_T[2].style.color="green"
+
+// document.querySelector(".this").style.color="yellow"
+// document.querySelector(".this").style.background="black"
+
+// console.log(document.getElementsByTagName("a"));
+// console.log(document.querySelector(".card").getElementsByTagName("a"));
+console.log(document.getElementsByName("search"));
