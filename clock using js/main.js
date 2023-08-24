973015f2198f47a1b4ef76cfac3b3e8a304238bd
Anuvab-Chakraborty <anuvab.xd@gmail.com>
2023-08-24 10:41:32 +0530

    CLOCK USING JS

--- clock using js/main.js
+++ clock using js/main.js
@@ -17,15 +17,15 @@ if (date_now<10){date_now=`0${date_now}`}
 let month=constructor.getMonth()
 if(month<10){month=`0${month+1}`}
 let year=constructor.getFullYear()
-week_arr=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+week_arr=["Sun","Mon","Tue","Wed","Thu","Fri","Sat"]
 week_day=week_arr[day]
 let d_m_y=document.body.getElementsByClassName("date_month_year")[0]
-d_m_y.innerHTML=`${date_now} / ${month} /${year}`
+d_m_y.innerHTML=`<h3>${date_now} - ${month} - ${year}</h3>`
 //console.log(d_m_y);
 let h_m_s=document.body.getElementsByClassName("hour_minute_sec")[0]
-h_m_s.innerHTML=`${hours} : ${minutes} : ${seconds}`
+h_m_s.innerHTML=`<h1>${hours} : ${minutes} : ${seconds}</h1>`
 let w_d=document.body.getElementsByClassName("week_day")[0]
-w_d.innerHTML=`${week_day}`
+w_d.innerHTML=`<h3>${week_day}</h3>`
 }
 //date_get()
 setInterval(date_get,1000)
